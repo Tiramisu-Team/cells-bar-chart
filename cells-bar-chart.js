e79663590da63c0baddfb009440ceb7f2f08f8fc
@ninja.arg <ninja@ubuntu>
2018-09-14 14:58:12 -0300

    Cambio en las variables de entrada del componente

--- cells-bar-chart.js
+++ cells-bar-chart.js
@@ -6,24 +6,49 @@ class CellsBarChart extends Polymer.Element {
 
   static get properties() {
     return {
-      data: {
-        type: Array,
-        value: [{
-          "name": "Chrome",
-          "data": [62.74,5]
-      },
-      {
-          "name": "Firefox",
-          "data": [70,5]
-          
-      }]
-      },
-      xAxis: {
+      detail: {
         type: Object,
-        value: {categories:["category","lalalallalallaal00"],crosshair: true}
+        observer: '_parseDetail'
+      },
+      _data: {
+        type: Array,
+        value: []
+      },
+      _categories: {
+        type: Array
+      },
+      _xAxis: {
+        type: Object,
+        computed: '_compute_x_Axis(_categories)',
+      },
+      yLabel: {
+        type: String
+      },
+      subtitle: {
+        type: String
+      },
+      title: {
+        type: String
+      },
+      xLabel: {
+        type: String
+      },
+      label: {
+        type: String
       }
     };
   }
+
+  _parseDetail(det) {
+    this.set('_categories', det.categories);
+    this.set('_data', det.data);
+  }
+
+  _compute_x_Axis(_categories) {
+    return {categories: _categories, crosshair: true};
+  }
+
+  
 }
 
 customElements.define(CellsBarChart.is, CellsBarChart);
