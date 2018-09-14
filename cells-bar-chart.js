5b7878136f59561e1726f19f13f4750c8a3be766
@ninja.arg <ninja@ubuntu>
2018-09-13 23:22:52 -0300

    initial commit

---
+++ cells-bar-chart.js
@@ -0,0 +1,12 @@
+class CellsBarChart extends Polymer.Element {
+
+  static get is() {
+    return 'cells-bar-chart';
+  }
+
+  static get properties() {
+    return {};
+  }
+}
+
+customElements.define(CellsBarChart.is, CellsBarChart);
